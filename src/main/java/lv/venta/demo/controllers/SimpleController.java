a0efbcaada4a620ea7d0f5aee72c8fe298914262
Karina Skirmante <karina.krinkele@venta.lv>
2022-04-14 14:27:17 +0300

    Edit: added new controoler with String msg

--- src/main/java/lv/venta/demo/controllers/SimpleController.java
+++ src/main/java/lv/venta/demo/controllers/SimpleController.java
@@ -1,6 +1,7 @@
 package lv.venta.demo.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
@@ -11,6 +12,14 @@ public class SimpleController {
 	{
 		System.out.println("Kontrolieris nostrādāja");
 		return "home";//ielādēs home.html lapu
+	}
+	
+	@GetMapping("/msg")
+	public String msgFunc(Model model)//vjaga, lai sūtītu datus no backend uz frontend
+	{
+		model.addAttribute("package", "Ziņa no JAVA Backend puses");
+		System.out.println("msg kontrolieris ir izasukts");
+		return "msg-page";//ielādējam msg-page.html lapu
 	}
 	
 }
