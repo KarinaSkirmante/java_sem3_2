8157d66fd585a4b8e678c09dabebb612d76eb566
Karina Skirmante <karina.krinkele@venta.lv>
2022-04-14 14:50:47 +0300

    Edit: add controller with object sending

--- src/main/java/lv/venta/demo/controllers/SimpleController.java
+++ src/main/java/lv/venta/demo/controllers/SimpleController.java
@@ -4,6 +4,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import lv.venta.demo.model.Product;
+
 @Controller
 public class SimpleController {
 
@@ -21,5 +23,14 @@ public class SimpleController {
 		System.out.println("msg kontrolieris ir izasukts");
 		return "msg-page";//ielādējam msg-page.html lapu
 	}
+	
+	@GetMapping("/object")//localhost:8080/object
+	public String objectFunc(Model model)
+	{
+		Product product = new Product("Abols", 0.99f, 100);
+		model.addAttribute("package", product);
+		return "prod-page";//ielādējām prod-page.html lapu ar package sūtījumu
+	}
+	
 	
 }
